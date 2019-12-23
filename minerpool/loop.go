8dde2adbe6f4abfea8ec0bf040ca288d77f205bb
jojoin <yangjie@jojoin.com>
2019-12-23 22:48:29 +0800

    fix bug

--- minerpool/loop.go
+++ minerpool/loop.go
@@ -5,10 +5,12 @@ func (p *MinerPool) loop() {
 	for {
 		select {
 		case obj := <-p.successFindBlockCh:
-			if obj.msg.BlockHeadMeta.GetHeight() <= p.currentSuccessFindBlockHeight {
-				break
-			}
-			p.currentSuccessFindBlockHeight = obj.msg.BlockHeadMeta.GetHeight()
+			/*
+				if obj.msg.BlockHeadMeta.GetHeight() <= p.currentSuccessFindBlockHeight {
+					break
+				}
+				p.currentSuccessFindBlockHeight = obj.msg.BlockHeadMeta.GetHeight()
+			*/
 			// do add success
 			obj.account.successFindNewBlock(obj.msg)
 		}
