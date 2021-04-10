bd97fa843697af7dfd38411907defd4f6506c1b4
jojoin <yangjie@jojoin.com>
2021-04-10 10:48:42 +0800

    up version

--- run/main/main.go
+++ run/main/main.go
@@ -37,16 +37,16 @@ go build -o test/pcwallet pcwallet/main/main.go  && ./test/pcwallet
 
 编译发布版本：
 
-go build -ldflags '-w -s' -o              hacash_node_2021_03_25_01  miner/run/main/main.go
-go build -ldflags '-w -s' -o hacash_miner_pool_worker_2021_03_25_01  miner/run/minerpoolworker/main.go
-go build -ldflags '-w -s' -o         hacash_cmdwallet_2021_03_25_01  cmdwallet/run/main/main.go
+go build -ldflags '-w -s' -o              hacash_node_2021_04_10_01  miner/run/main/main.go
+go build -ldflags '-w -s' -o hacash_miner_pool_worker_2021_04_10_01  miner/run/minerpoolworker/main.go
+go build -ldflags '-w -s' -o         hacash_cmdwallet_2021_04_10_01  cmdwallet/run/main/main.go
 
 cd ./x16rs/opencl && node pkgclfilego.js && cd ../../
-go build -ldflags '-w -s' -o hacash_miner_worker_2021_03_24_01  miner/run/minerworker/main.go
+go build -ldflags '-w -s' -o hacash_miner_worker_2021_04_10_01  miner/run/minerworker/main.go
 
-go build -ldflags '-w -s' -o hacash_miner_relay_service_2021_04_02 miner/run/minerrelayservice/main.go
+go build -ldflags '-w -s' -o hacash_miner_relay_service_2021_04_10_01 miner/run/minerrelayservice/main.go
 
-go build -ldflags '-w -s' -o hacash_desktop_offline_wallet_2021_03_25_01  pcwallet/main/main.go
+go build -ldflags '-w -s' -o hacash_desktop_offline_wallet_2021_04_10_01  pcwallet/main/main.go
 
 
 */
@@ -55,8 +55,8 @@ const (
 	NodeVersionSuperMain    uint32 = 0            // 主版本号
 	NodeVersionSupport      uint32 = 1            // 兼容版本号
 	NodeVersionFeature      uint32 = 2            // 特征版本号
-	NodeVersionBuildCompile string = "20210325.1" // 编译版本号
-	// 结合成综合版本号体系：   0.1.1(20210310.1)
+	NodeVersionBuildCompile string = "20210410.1" // 编译版本号
+	// 结合成综合版本号体系：   0.1.2(20210410.1)
 )
 
 func main() {
