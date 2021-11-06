b99e8622cfe340a91903703912e82cc4aa2aaea6
yangjie <yangjie@jojoin.com>
2021-11-06 21:37:28 +0800

    chg all fyne/v1 => fyne/v2

--- run/main/main.go
+++ run/main/main.go
@@ -42,17 +42,17 @@ go build -o test/poolworkertest1 miner/run/minerpoolworker/main.go  && ./test/po
 
 编译发布版本：
 
-go build -ldflags '-w -s' -o                   hacash_node_2021_11_01_01  miner/run/main/main.go
-go build -ldflags '-w -s' -o      hacash_miner_pool_worker_2021_11_01_01  miner/run/minerpoolworker/main.go
-go build -ldflags '-w -s' -o    hacash_miner_relay_service_2021_11_01_01  miner/run/minerrelayservice/main.go
-go build -ldflags '-w -s' -o hacash_desktop_offline_wallet_2021_11_01_01  pcwallet/main/main.go
-go build -ldflags '-w -s' -o              hacash_cmdwallet_2021_11_01_01  cmdwallet/run/main/main.go
+go build -ldflags '-w -s' -o                   hacash_node_2021_11_06_01  miner/run/main/main.go
+go build -ldflags '-w -s' -o      hacash_miner_pool_worker_2021_11_06_01  miner/run/minerpoolworker/main.go
+go build -ldflags '-w -s' -o    hacash_miner_relay_service_2021_11_06_01  miner/run/minerrelayservice/main.go
+go build -ldflags '-w -s' -o hacash_desktop_offline_wallet_2021_11_06_01  pcwallet/main/main.go
+go build -ldflags '-w -s' -o              hacash_cmdwallet_2021_11_06_01  cmdwallet/run/main/main.go
 
-go build -ldflags '-w -s' -o    hacash_channelpay_servicer_2021_11_01_01  channelpay/run/servicer/main.go
-go build -ldflags '-w -s' -o      hacash_channelpay_client_2021_11_01_01  channelpay/run/client/main.go
+go build -ldflags '-w -s' -o    hacash_channelpay_servicer_2021_11_06_01  channelpay/run/servicer/main.go
+go build -ldflags '-w -s' -o      hacash_channelpay_client_2021_11_06_01  channelpay/run/client/main.go
 
 cd ./x16rs/opencl && node pkgclfilego.js && cd ../../
-go build -ldflags '-w -s' -o           hacash_miner_worker_2021_11_01_01  miner/run/minerworker/main.go
+go build -ldflags '-w -s' -o           hacash_miner_worker_2021_11_06_01  miner/run/minerworker/main.go
 
 
 */
@@ -61,8 +61,8 @@ const (
 	NodeVersionSuperMain    uint32 = 0            // 主版本号
 	NodeVersionSupport      uint32 = 1            // 兼容版本号
 	NodeVersionFeature      uint32 = 7            // 特征版本号
-	NodeVersionBuildCompile string = "20211101.1" // 编译版本号
-	// 结合成综合版本号体系：   0.1.7(20211101.1)
+	NodeVersionBuildCompile string = "20211106.1" // 编译版本号
+	// 结合成综合版本号体系：   0.1.7(20211106.1)
 )
 
 /**
